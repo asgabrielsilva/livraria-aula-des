a6d9346bcb560bc26dca9907e8d5186a70729164
asgabrielsilva <gabrielalmeida290905@gmail.com>
2024-08-19 14:35:30 -0300

    Upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -19,6 +21,8 @@ router.register(r"editoras", EditoraViewSet, basename="editoras")
 router.register(r"livros", LivroViewSet, basename="livros")
 router.register(r"users", UserViewSet, basename="users")
 
+from uploader.router import router as uploader_router
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     # OpenAPI 3
@@ -36,6 +40,11 @@ urlpatterns = [
     # Simple JWT
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Uploader
+    path("api/media/", include(uploader_router.urls)),  # nova linha
+
     # API
     path("api/", include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
